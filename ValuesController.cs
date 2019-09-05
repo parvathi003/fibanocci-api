481c5558d4ca1f2ebbb0a2090b90c0b4798d6b19
parvathi003 <51682284+parvathi003@users.noreply.github.com>
2019-09-05 17:24:53 +0530

    Add files via upload

---
+++ ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            int f = 0;
+            int s = 1;
+            int r = 0;
+            string series = "";
+            for (int i = 0; i < 10; i++)
+            {
+                series = series + " " + s.ToString();
+                f = s;
+                s = r;
+                r = s + f;
+            }
+
+
+            return series;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
